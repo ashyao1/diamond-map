be54e50ee081610219bebd2330e7663c6781f77d
ashyao1 <acyyao@gmail.com>
2026-06-30 11:22:29 +0100

    Adding map of DLS

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ function alerthello() {
     text1 = "I have " + totalnum
     alert(text1)
 }
+
+var map = L.map('map').setView([51.574349, -1.310892], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.574349, -1.310892]).addTo(map);
